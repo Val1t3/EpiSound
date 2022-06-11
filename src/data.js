4ddb32ab971102bb48fb7bba05eeb0f36e871528
Valent1 <valentin.woehrel@epitech.eu>
2022-06-11 07:55:29 +0200

    [ADD] Alexis + sound for Valentin

--- src/data.js
+++ src/data.js
@@ -1,17 +1,22 @@
 export const stockData = [
     {
         "id": "1",
+        "name": "Alexis",
+        "src": "./assets/sounds/miaou.mp3"
+    },
+    {
+        "id": "2",
         "name": "Arthur",
         "src": "./assets/sounds/feur.mp3"
     },
     {
-        "id": "2",
+        "id": "3",
         "name": "Grégoire",
         "src": "./assets/sounds/malloc.mp3"
     },
     {
-        "id": "3",
+        "id": "4",
         "name": "Valentin",
-        "src": "./assets/sounds/feur.mp3"
+        "src": "./assets/sounds/quoi.mp3"
     }
 ];
